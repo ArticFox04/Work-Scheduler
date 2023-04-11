d9acf252d16d15b1da81518a905f00e44a0be6b5
Christian Loera <114829265+ArticFox04@users.noreply.github.com>
2023-04-10 23:26:51 -0600

    added the past,present and future functions still working on making it work on for present and past

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function () {
-  $("#currentDay").text(dayjs().format('dddd, MMMM D, YYYY'));
+  $("#currentDay").text(dayjs().format('MMMM D, YYYY, dddd, hh:mm:ss'));
   
   $(function () {
 
@@ -8,7 +8,17 @@ $(function () {
     $(".saveBtn").on("click", function() {
       localStorage.setItem($(this).parent().attr("id"), $(this).prev().val());
     });
-  
+    for (let i = 0; i < htmlHourDivs.length; i++) {
+      let divId = htmlHourDivs[i].id;
+      let divHour = divId[1];
+      if (divHour < currentHour) {
+        $("#" + divId).addClass("past");
+      } else if (divHour === currentHour) {
+        $("#" + divId).addClass("present");
+      } else  {
+        $("#" + divId).addClass("future");
+      }
+    }
   
 
 
